9f8e32afbd88d861a9aa75c4612c14880c44a827
Your Name <you@example.com>
2023-12-08 19:54:53 -0600

    aider: Updated `main()` function in `ai_agent/main.py` to import necessary modules, connect to the database, get tables in prompt-ready form, update the prompt, generate a response, parse the SQL response, and run the parsed SQL.
    # Aider chat conversation:
    
    ASSISTANT: To update the `main()` function in `ai_agent/main.py` based on the comments, we need to:
    
    1. Import the necessary modules.
    2. Connect to the database using the `MySQLDatabase` class.
    3. Get the tables in a prompt-ready form using the `get_table_definitions_for_prompt()` method.
    4. Update the prompt by calling `add_cap_ref()` twice.
    5. Generate a response from the prompt using the `prompt()` function.
    6. Parse the SQL response from the `prompt_response` using a delimiter.
    7. Run the parsed SQL using the `run_sql()` method of the `MySQLDatabase` class.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    ai_agent/main.py
    ```python
    <<<<<<< SEARCH
    from ai_agent.modules.db import MySQLDatabase
    import os
    import dotenv
    
    dotenv.load_dotenv()
    assert os.environ.get("DATABASE_URL"), "DB_URL not found in .env file"
    assert os.environ.get("AZURE_OPENAI_KEY"), "key not found in .env file"
    assert os.environ.get("AZURE_OPENAI_ENDPOINT"), "endpoint not found in .env file"
    
    DB_URL = os.environ.get("DATABASE_URL")
    AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY")
    AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT")
    
    def main():
        # parse prompt param using arg parse
    
        # connect to db using with statement and create a db_manager
        with MySQLDatabase() as db:
            db.connect_with_url(DB_URL)
            user_table = db.get_all("CUSTOMER")
            print("user_tables", user_table)
        # call db_manager-get_table_definition_for_prompt() to get tables in prompt ready form
    
        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
    
        # call 1lm.prompt to get a prompt_response variable
    
        # parse sql response from prompt_response using SQL_QUERY_DELIMITER
    
        # call db_manager.run_sql() with the parsed sql
        pass
    
    if __name__ == "__main__":
        main()
    =======
    from ai_agent.modules.db import MySQLDatabase
    from ai_agent.modules.llm import add_cap_ref, prompt
    import os
    import dotenv
    
    dotenv.load_dotenv()
    assert os.environ.get("DATABASE_URL"), "DB_URL not found in .env file"
    assert os.environ.get("AZURE_OPENAI_KEY"), "key not found in .env file"
    assert os.environ.get("AZURE_OPENAI_ENDPOINT"), "endpoint not found in .env file"
    
    DB_URL = os.environ.get("DATABASE_URL")
    AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY")
    AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT")
    
    def main():
        # parse prompt param using arg parse
    
        # connect to db using with statement and create a db_manager
        with MySQLDatabase() as db:
            db.connect_with_url(DB_URL)
            user_table = db.get_all("CUSTOMER")
            print("user_tables", user_table)
    
            # call db_manager-get_table_definition_for_prompt() to get tables in prompt ready form
            tables_prompt = db.get_table_definitions_for_prompt()
    
            # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
            prompt_with_tables = add_cap_ref(prompt, "", "TABLES", tables_prompt)
            prompt_with_tables_and_sql = add_cap_ref(prompt_with_tables, "", "SQL", "")
    
            # call llm.prompt to get a prompt_response variable
            prompt_response = prompt(prompt_with_tables_and_sql)
    
            # parse sql response from prompt_response using SQL_QUERY_DELIMITER
            sql_queries = prompt_response.split(SQL_QUERY_DELIMITER)
    
            # call db_manager.run_sql() with the parsed sql
            for query in sql_queries:
                db.run_sql(query.strip())
    
        pass
    
    if __name__ == "__main__":
        main()
    >>>>>>> REPLACE
    ```
    
    Please note that the `SQL_QUERY_DELIMITER` is not defined in the code you provided. You will need to define it before running the updated code.

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -1,4 +1,5 @@
 from ai_agent.modules.db import MySQLDatabase
+from ai_agent.modules.llm import add_cap_ref, prompt
 import os
 import dotenv
 
@@ -20,15 +21,24 @@ def main():
         db.connect_with_url(DB_URL)
         user_table = db.get_all("CUSTOMER")
         print("user_tables", user_table)
-    # call db_manager-get_table_definition_for_prompt() to get tables in prompt ready form
-
-    # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
-
-    # call 1lm.prompt to get a prompt_response variable
-
-    # parse sql response from prompt_response using SQL_QUERY_DELIMITER
-
-    # call db_manager.run_sql() with the parsed sql
+        
+        # call db_manager-get_table_definition_for_prompt() to get tables in prompt ready form
+        tables_prompt = db.get_table_definitions_for_prompt()
+        
+        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
+        prompt_with_tables = add_cap_ref(prompt, "", "TABLES", tables_prompt)
+        prompt_with_tables_and_sql = add_cap_ref(prompt_with_tables, "", "SQL", "")
+        
+        # call llm.prompt to get a prompt_response variable
+        prompt_response = prompt(prompt_with_tables_and_sql)
+        
+        # parse sql response from prompt_response using SQL_QUERY_DELIMITER
+        sql_queries = prompt_response.split(SQL_QUERY_DELIMITER)
+        
+        # call db_manager.run_sql() with the parsed sql
+        for query in sql_queries:
+            db.run_sql(query.strip())
+            
     pass
 
 
